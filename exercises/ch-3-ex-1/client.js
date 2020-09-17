fa8b37b771643ebb30380d1a752592ef370e2d99
tamu222i <t.a.m.u.2.2.2.i@gmail.com>
2020-09-17 04:43:17 +0000

    get token

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -29,6 +29,8 @@ var authServer = {
 var client = {
 	"client_id": "oauth-client-1",
 	"client_secret": "oauth-client-secret-1",
+	//"client_id": "oauth-client-1",
+	//"client_secret": "oauth-client-secret-1",
 	"redirect_uris": ["http://localhost:9000/callback"]
 };
 
@@ -62,7 +64,24 @@ app.get('/callback', function(req, res){
 	/*
 	 * Parse the response from the authorization server and get a token
 	 */
-	
+	var code = req.query.code;
+	var form_data = qs.stringify({
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: client.redirect_uris[0]
+	});
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
+	};
+	var tokRes = request('POST', authServer.tokenEndpoint,
+		{
+			body: form_data,
+			headers: headers
+		}
+	);
+	var body = JSON.parse(tokRes.getBody());
+	res.render('index', { access_token: body.access_token, scope: scope});
 });
 
 app.get('/fetch_resource', function(req, res) {
