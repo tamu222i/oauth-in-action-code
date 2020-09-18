8fb8c666ecb4cd9ef81acfc89f784522f44cdfa5
tamu222i <t.a.m.u.2.2.2.i@gmail.com>
2020-09-18 05:11:15 +0000

    add scope

--- exercises/ch-4-ex-3/protectedResource.js
+++ exercises/ch-4-ex-3/protectedResource.js
@@ -59,18 +59,30 @@ var requireAccessToken = function(req, res, next) {
 };
 
 app.get('/produce', getAccessToken, requireAccessToken, function(req, res) {
+	/*
 	var produce = {fruit: ['apple', 'banana', 'kiwi'], 
 		veggies: ['lettuce', 'onion', 'potato'], 
 		meats: ['bacon', 'steak', 'chicken breast']};	
+	*/
 
 	/*
 	 * Add different kinds of produce based on the incoming token's scope
 	 */
+	var produce = {fruit: [], veggies: [], meats: []};
+	if (__.contains(req.access_token.scope, 'fruit')) {
+		produce.fruit = ['apple', 'banana', 'kiwi'];
+	}
+	if (__.contains(req.access_token.scope, 'veggies')) {
+		produce.veggies = ['lettuce', 'onion', 'potato'];
+	}
+	if (__.contains(req.access_token.scope, 'meats')) {
+		produce.meats = ['bacon', 'steak', 'chicken breast'];
+	}
 
 	res.json(produce);
 });
 
-var server = app.listen(9002, 'localhost', function () {
+var server = app.listen(9002, '0.0.0.0', function () {
   var host = server.address().address;
   var port = server.address().port;
 
