740a0e262d2e761cff4dfda74fa3af292a27d10e
tamu222i <t.a.m.u.2.2.2.i@gmail.com>
2020-09-18 05:02:50 +0000

    add check access_token

--- exercises/ch-4-ex-1/protectedResource.js
+++ exercises/ch-4-ex-1/protectedResource.js
@@ -27,6 +27,29 @@ var getAccessToken = function(req, res, next) {
 	 * Scan for an access token on the incoming request.
 	 */
 	
+	var inToken = null;
+	var auth = req.headers['authorization'];
+	if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
+		inToken = auth.slice('bearer '.length);
+	} else if (req.body && req.body.access_token) {
+		inToken = req.body.access_token;
+	} else if (req.query && req.query.access_token) {
+		inToken = req.query.access_token;
+	}
+	nosql.one(function(token) {
+		if (token.access_token == inToken) {
+			return token;
+		}
+	}, function(err, token) {
+		if (token) {
+			console.log("We found  a matching token: %s", inToken);
+		} else {
+			console.log("no matching token was found");
+		}
+		req.access_token = token;
+		next();
+		return;
+	});
 };
 
 app.options('/resource', cors());
@@ -35,15 +58,21 @@ app.options('/resource', cors());
 /*
  * Add the getAccessToken function to this handler
  */
-app.post("/resource", cors(), function(req, res){
+//app.post("/resource", cors(), function(req, res){
+app.post("/resource", getAccessToken, function(req, res){
 
 	/*
 	 * Check to see if the access token was found or not
 	 */
 	
+	if (req.access_token) {
+		res.json(resource);
+	} else {
+		res.status(401).end();
+	}
 });
 
-var server = app.listen(9002, 'localhost', function () {
+var server = app.listen(9002, '0.0.0.0', function () {
   var host = server.address().address;
   var port = server.address().port;
 
